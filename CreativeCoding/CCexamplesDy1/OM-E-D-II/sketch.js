b4c16dd5cae0dbb6e188ab73d504a20b669c4cc0
eeriley99 <eeriley99@gmail.com>
2023-01-02 17:10:15 -0500

    Update sketch.js

--- CreativeCoding/CCexamplesDy1/OM-E-D-II/sketch.js
+++ CreativeCoding/CCexamplesDy1/OM-E-D-II/sketch.js
@@ -3,7 +3,7 @@ function preload (){
   window1 = loadImage ('Images/DayWindow3.png');
   window2 = loadImage ('Images/DayWindow2.png');
   window3 = loadImage ('Images/DayWindow4.png');
-  window4 = loadImage ('Images/DayWindow5.png');
+  window4 = loadImage ('Images/DayWindow1.png');
 
 }
 
