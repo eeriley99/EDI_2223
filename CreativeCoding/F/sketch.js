b479126d53e0cd7daa97ee2c5efa2652eb410c7d
eeriley99 <eeriley99@gmail.com>
2023-01-09 08:48:10 -0500

    Update sketch.js

--- CreativeCoding/F/sketch.js
+++ CreativeCoding/F/sketch.js
@@ -13,19 +13,16 @@ function setup() { //runs once
 function draw() {
 background (255);
 
-translate(-100, -100)
+translate(-100, -100)//to give a bleed effect
 
 var num = 57; //variable for the number of squares in the array
 var sideLen = windowWidth/num;//variable for the side lenth of each square
 
   for (var y = 0; y < 2*windowHeight; y = y + sideLen) {//loop to create rows in the y direction
      for (var x = 0; x < 2*windowWidth; x = x + sideLen) {// loop to create a row of squares in the x direction0
-    image(gif, x, y, windowWidth*.9/num, windowWidth*.9/num);
-
-
+    image(gif, x, y, windowWidth/num, windowWidth/num);
   }
 }
-
 }
 
 function windowResized() {
