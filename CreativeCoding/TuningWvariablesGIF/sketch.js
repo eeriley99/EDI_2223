8bee3e076a7098397ee3602c7df723875e4cb9e9
eeriley99 <eeriley99@gmail.com>
2023-01-09 00:40:43 -0500

    Update sketch.js

--- CreativeCoding/TuningWvariablesGIF/sketch.js
+++ CreativeCoding/TuningWvariablesGIF/sketch.js
@@ -5,17 +5,6 @@ function preload() {
 
 function setup() {//runs once
   createCanvas(windowWidth , windowHeight*2);
-}
-
-
-
-function draw() {//runs in a loop
-
-
-  background (255,255,255);
-
-
-
   var num = 10; //varible for the number of squares in the array
 
    var sideLen = windowWidth/num; //variable for the side length of each square
@@ -26,7 +15,15 @@ image(gif,x,y, windowWidth/num,windowWidth/num);
 
 }
 }
+}
+
+
+
+function draw() {//runs in a loop
+
+
   }
+
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight*3);
 }
