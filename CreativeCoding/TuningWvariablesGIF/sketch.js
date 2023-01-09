4a95969bfefdaa21497e03364fa14cc59920b300
eeriley99 <eeriley99@gmail.com>
2023-01-09 00:05:27 -0500

    Update sketch.js

--- CreativeCoding/TuningWvariablesGIF/sketch.js
+++ CreativeCoding/TuningWvariablesGIF/sketch.js
@@ -17,9 +17,9 @@ var num = 10; //variable for the number of squares in the array
 
   var sideLen = windowWidth/num; //variable for the side length of each square
 
-  for (var y = 0; y < windowWidth; y = y + sideLen) {//loop to create rows in the y direction
+  for (var y = 0; y < 2*windowWidth; y = y + sideLen) {//loop to create rows in the y direction
 
-     for (var x = 0; x < windowWidth; x = x + sideLen) {// loop to create a row of squares in the x direction
+     for (var x = 0; x < 2*windowWidth; x = x + sideLen) {// loop to create a row of squares in the x direction
 
   quad(x, y,
     x + sideLen, y,
@@ -32,7 +32,7 @@ var num = 10; //variable for the number of squares in the array
   }
 
   }
-noLoop ();
+
 }
 
 function windowResized() {
