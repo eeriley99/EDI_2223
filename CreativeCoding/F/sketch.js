cdfa61d1eb8314b5aea4e92b95a64cc209cee08a
eeriley99 <53475374+eeriley99@users.noreply.github.com>
2023-01-09 08:09:34 -0500

    f_gif

--- CreativeCoding/F/sketch.js
+++ CreativeCoding/F/sketch.js
@@ -1,7 +1,29 @@
-function setup() {
+let gif;
 
+function preload (){
+  gif = loadImage ('yellowSquare.gif');
+}
+
+function setup() { //runs once
+  createCanvas(windowWidth, windowHeight*2);
+  fill('indigo');
+  strokeWeight(1);
 }
 
 function draw() {
+background (255);
+translate(-100, -100)
 
+var num = 57; //variable for the number of squares in the array
+var sideLen = windowWidth/num;//variable for the side lenth of each square
+
+  for (var y = 0; y < 2*windowHeight; y = y + sideLen) {//loop to create rows in the y direction
+     for (var x = 0; x < 2*windowWidth; x = x + sideLen) {// loop to create a row of squares in the x direction0
+    image(gif, x, y, windowWidth*.9/num, windowWidth*.9/num);
+  }
+}
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
