6fd6b4c425bf4cd37bd4ce08b673245dc08e43e2
eeriley99 <eeriley99@gmail.com>
2023-01-09 08:24:18 -0500

    js update

--- CreativeCoding/F/sketch.js
+++ CreativeCoding/F/sketch.js
@@ -1,7 +1,7 @@
 let gif;
 
 function preload (){
-  gif = loadImage ('yellowSquare.gif');
+  gif = loadImage ('Assets_2/SkullGif2.gif');
 }
 
 function setup() { //runs once
@@ -12,6 +12,7 @@ function setup() { //runs once
 
 function draw() {
 background (255);
+
 translate(-100, -100)
 
 var num = 57; //variable for the number of squares in the array
@@ -20,8 +21,11 @@ var sideLen = windowWidth/num;//variable for the side lenth of each square
   for (var y = 0; y < 2*windowHeight; y = y + sideLen) {//loop to create rows in the y direction
      for (var x = 0; x < 2*windowWidth; x = x + sideLen) {// loop to create a row of squares in the x direction0
     image(gif, x, y, windowWidth*.9/num, windowWidth*.9/num);
+
+
   }
 }
+
 }
 
 function windowResized() {
