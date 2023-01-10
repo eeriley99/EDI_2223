e53c99a276b164aa8ba1f83fc9dc8b73985db729
eeriley99 <53475374+eeriley99@users.noreply.github.com>
2023-01-10 10:27:22 -0500

    Update sketch.js

--- CreativeCoding/VariablesGIF/sketch.js
+++ CreativeCoding/VariablesGIF/sketch.js
@@ -8,7 +8,7 @@ function preload (){
 
 function setup() {//runs once
 createCanvas (windowWidth, windowWidth);// creates a canvas 720 pixels by 720 pixels
-background (255, 255, 0); //background color
+background (255, 0, 0); //background color
 }
 
 function draw() {//runs in a loop
@@ -18,11 +18,16 @@ strokeWeight (1);//assigns stroke weight
 var num = 30; // number of squares in my array
 var sideLen = windowWidth/num; //side length
 
+translate (-100, -100); //translation creates a bleed effect
 
-for (var y = 0; y < windowWidth; y = y + sideLen ){//loop creates a row in the y direction
-for (var x = 0; x  < windowWidth; x = x + sideLen ){//loop creates a row in the x direction
+for (var y = 0; y < 2 * windowWidth; y = y + sideLen ){//loop creates a row in the y direction
+for (var x = 0; x  < 2 * windowWidth; x = x + sideLen ){//loop creates a row in the x direction
 
 image (gif, x, y, windowWidth/num, windowWidth/num);
 }
 }
+}
+
+function windowResized () {
+  resizeCanvas (windowWidth, windowHeight);
 }
